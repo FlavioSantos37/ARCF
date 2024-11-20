acfb3d8c6bfa958dd91a34cabbf548ee7acea609
FlavioSantos37 <desenvolvedorbr37@gmail.com>
2024-11-20 00:29:31 -0300

    2011

--- app.js
+++ app.js
@@ -71,3 +71,17 @@ class FormSubmit {
     }, 500);        
     return false;
 });
+
+
+function toggleMenu() {
+  const sidebar = document.getElementById('menu');
+  const mainContent = document.querySelector('.container');
+
+  if (sidebar.style.width === '250px') {
+    sidebar.style.width = '0';
+    mainContent.style.marginLeft = '0';
+  } else {
+    sidebar.style.width = '250px';
+    mainContent.style.marginLeft = '250px';
+  }
+}
